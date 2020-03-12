a633dabb7a80f30d5984ab27e16b87c370c08881
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-03-12 19:55:17 +0530

    v1
    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.account_balance),
+            onPressed: () {},
+          ),
+          centerTitle: true,
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
